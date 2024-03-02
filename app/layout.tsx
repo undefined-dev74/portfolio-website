ddcd8278bc94e5fa077e9c73739f80b440327d60
undefined-dev74 <aman.kumar.jha953@gmail.com>
2024-03-02 21:36:00 +0530

    fix : changed meta data and tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
-import Header from "@/components/header";
-import "./globals.css";
-import { Inter } from "next/font/google";
-import ActiveSectionContextProvider from "@/context/active-section-context";
 import Footer from "@/components/footer";
+import Header from "@/components/header";
 import ThemeSwitch from "@/components/theme-switch";
+import ActiveSectionContextProvider from "@/context/active-section-context";
 import ThemeContextProvider from "@/context/theme-context";
+import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "Aman | Personal Portfolio",
+  description: "Aman is a React developer with 2.6 years of experience.",
 };
 
 export default function RootLayout({
